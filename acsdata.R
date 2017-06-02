bdc55c7c5a1713c2dce21ea733f17c533694b35e
Dongwoo Yang <dongwooyang@outlook.com>
2017-06-02 18:23:18 +0900

    acs data coding

---
+++ acsdata.R
@@ -0,0 +1,26 @@
+# install.packages("RPostgreSQL")
+require("RPostgreSQL")
+ 
+# create a connection
+# loads the PostgreSQL driver
+drv <- dbDriver("PostgreSQL")
+# creates a connection to the postgres database
+# note that "con" will be used later in each connection to the database
+con <- dbConnect(drv, dbname = "acs",
+                 host = "localhost", port = 5432,
+                 user = "postgres", password = '6344vppv')
+
+# query the data from postgreSQL 
+geo_im <- dbGetQuery(con, "SELECT * from geo_ca WHERE geoid LIKE '%5000US06025%' AND geoname LIKE '%Block-Group%'")
+geo_la <- dbGetQuery(con, "SELECT * from geo_ca WHERE geoid LIKE '%5000US06037%' AND geoname LIKE '%Block-Group%'")
+geo_rs <- dbGetQuery(con, "SELECT * from geo_ca WHERE geoid LIKE '%5000US06065%' AND geoname LIKE '%Block-Group%'")
+geo_oc <- dbGetQuery(con, "SELECT * from geo_ca WHERE geoid LIKE '%5000US06059%' AND geoname LIKE '%Block-Group%'")
+geo_sb <- dbGetQuery(con, "SELECT * from geo_ca WHERE geoid LIKE '%5000US06071%' AND geoname LIKE '%Block-Group%'")
+geo_vn <- dbGetQuery(con, "SELECT * from geo_ca WHERE geoid LIKE '%5000US06111%' AND geoname LIKE '%Block-Group%'")
+geo_scag <- rbind(geo_im, geo_la, geo_rs, geo_oc, geo_sb, geo_vn)
+
+
+seq023_ca <- dbGetQuery(con, "SELECT * from seq023_tr)
+
+
+seq023_scag <- merge(geo_scag, seq023_ca, by="logrecno")
