7b9c94e9f58e07bb28adb3d202eb881c985908a4
Dongwoo Yang <dongwooyang@outlook.com>
2017-06-06 17:20:00 +0900

    getting transitscore from the address list

--- extractingTS.R
+++ extractingTS.R
@@ -7,7 +7,7 @@ library(rvest)
 library(XML)
 library(RDSTK)
 
-add<-read.table("//home//dongwoo//Desktop//address1.csv", header=TRUE)
+add<-read.table("//home//dongwoo//Desktop//address2.csv", header=TRUE)
 add$score<-NA
 #add$fadd<-NA
 #add$lat<-0
@@ -31,4 +31,4 @@ for (i in (1:nrow(add))) {
 )
 }
 
-write.table(add, "/home/dongwoo/Desktop/transitscore_ca1.csv", sep="\t")
+write.table(add, "/home/dongwoo/Desktop/transitscore_ca2.csv", sep="\t")
